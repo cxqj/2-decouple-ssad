7b98e9da39171e59cec5d68163ae4c9771604edd
cxqj <43702702+cxqj@users.noreply.github.com>
2020-06-14 20:38:15 +0800

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -68,11 +68,7 @@ def get_train_data(config, mode, pretrain_dataset, shuffle=True):
     gt_label_file = join(anno_path, 'gt_label.pkl') # 获取gt_label.pkl路径
     gt_info_file = join(anno_path, 'gt_info.pkl') # 获取gt_info.pkl路径，其中保存了归一化后的窗口的信息
     
-    #gt_label中记录了每个特征文件的属于20个类中的某个类
-    #gt_label的形式如下:[000000000100000],1代表对应的类别
     gt_label = read_pickle(gt_label_file)
-    #gt_info中保存了每个特征文件对应的信息，暂时不知道其中的意思
-    #gt_info的形式如下:[0.60125 1. 0.995]
     gt_info = read_pickle(gt_info_file)
 
     if not small_num_data_for_test:
@@ -80,26 +76,25 @@ def get_train_data(config, mode, pretrain_dataset, shuffle=True):
     else:
         num_data = batch_size
 
-    #预先定义一些列表存放数据，其中列表的每一个元素存放一个batch的数据
+    # 存放每一个batch对应的数据
     batch_dataX = []
     batch_gt_label = []
     batch_gt_info = []
     batch_index = []
 
-    #获取每个batch开始的索引,[0,32,64,......4704],共有4741个数据
+    
     batch_start_list = [i * batch_size for i in range(int(num_data / batch_size))]
     
-    # 4741-(4704+32) = 5 > (32/8 = 4)
-    # 满足条件则在后面追加batch开始的索引，最终的索引为[0,32,64.......4704,4709]
+    # 防止剩余的文件没有遍历到
     if (num_data - (batch_start_list[-1] + batch_size)) > (batch_size / 8):
         batch_start_list.append(num_data - batch_size)
     
     batch_shuffle_list = list(range(num_data))
     
-    # 一共4741个数据，则打乱他们的顺序以便后续加载一个batch时不用顺序加载
     if shuffle:
         random.seed(6)
         random.shuffle(batch_shuffle_list)  # 打乱数据的顺序
+        
     # 依次加载每一个batch的数据
     for bstart in batch_start_list:
         data_list = batch_shuffle_list[bstart:(bstart + batch_size)]  # 32个数据对应的索引 [424,1654,1621........]
@@ -109,20 +104,20 @@ def get_train_data(config, mode, pretrain_dataset, shuffle=True):
         tmp_batch_gt_info = []
         #根据下标获取对应数据，最后追加到对应列表
         for idx in data_list:
-            adataX = np.load(join(data_x_path, str(idx) + '.npy'))  # 得到数据的shape都为(128,1024)
+            adataX = np.load(join(data_x_path, str(idx) + '.npy'))  
             tmp_batch_dataX.append(adataX)
-            tmp_batch_gt_label.append(gt_label[idx]) # 需要看一下gt_label是怎么生成的，为啥每个还不一样
+            tmp_batch_gt_label.append(gt_label[idx]) 
             tmp_batch_gt_info.append(gt_info[idx]) 
 
         batch_dataX.append(np.array(tmp_batch_dataX))
         
-        #需要对获取到的一个batch的gt_label进行处理，具体是干嘛的需要细看
+        # 由于每个视频可能包含动作标注，这里就是将所有的标注去除，并生成新的起始索引
         tmp_batch_gt_label, start_index = batch_data_process(tmp_batch_gt_label)
         batch_gt_label.append(tmp_batch_gt_label)
         batch_index.append(start_index)
 
         tmp_batch_gt_info, start_index = batch_data_process(tmp_batch_gt_info)
-        batch_gt_info.append(tmp_batch_gt_info)  # 代码里以前不小心删除了这句话导致报错
+        batch_gt_info.append(tmp_batch_gt_info)
         
     #返回获取到所有的batch的数据，以列表形式存放，列表中每一个元素都存储了一个batch的数据
     return batch_dataX, batch_gt_label, batch_gt_info, batch_index
